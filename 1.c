e6e45644b80721cd17054d155159187d776af5ac
Jatin Dangi <100829623+jatindangi1206@users.noreply.github.com>
2022-09-26 18:18:01 +0530

    leave a line

--- 1.c
+++ 1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   
+   printf("Hello, World!\n");
    printf("Bye bye world!");
    return 0;
 }
