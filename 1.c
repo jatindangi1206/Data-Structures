c977784052d6867552a8dd147bc7d68daa1ce5f1
Jatin Dangi <100829623+jatindangi1206@users.noreply.github.com>
2022-09-27 12:16:33 +0530

    added one more line

--- 1.c
+++ 1.c
@@ -2,6 +2,7 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
-   printf("Bye bye world!");
+   printf("Bye bye world!\n");
+   printf("this is a test ");
    return 0;
 }
