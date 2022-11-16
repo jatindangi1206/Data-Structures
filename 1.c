ed90381ff243966855c32d5f8501f0b962a1b483
Jatin Dangi <100829623+jatindangi1206@users.noreply.github.com>
2022-11-16 11:22:57 +0530

    second

--- 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   printf("Bye bye world!\n");
-   printf("this is a test ");
-   return 0;
-}
--- practice/test1.c
+++ practice/test1.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+struct test1
+{
+    /* data */
+};
 
-int main (int argc, char *argv[]) {
+int main () {
+    struct node *ptr, *cpt;
+    ptr = head;
+    while (&ptr != NULL)
+    { 
+        cpt = &ptr;
+            while (cpt!=NULL)
+            { 
+                        if ( *ptr >  *cpt )
+                    {
+                        temp = *ptr;
+                        *ptr = *cpt;
+                        *cpt = temp;
+
+                        cpt = &cpt;
+
+                    }
+            
+                ptr = &ptr;
+            }
+    
+        
+    }
+    
+
+
+
     return 0;
 }
